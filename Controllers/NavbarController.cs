100a552dd45f05808aeb79968be6237423ba77f8
FrankWorks <frankfax@hotmail.com>
2016-09-06 22:47:15 -0700

    Initial Commit

---
+++ Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using DashBoard.Web.Domain;
+using DashBoard.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DashBoard.Web.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
